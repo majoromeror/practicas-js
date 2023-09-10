b7fdb4d291f35d7039d2d8619867aede578104b8
unknown <maria.jose.romero.rios@gmail.com>
2023-09-10 16:03:15 -0400

    Ejercicio angulo

---
+++ angulo.js
@@ -0,0 +1,7 @@
+console.log("GRADOS");
+inicial=Math.floor(Math.random()*360);
+valor=Math.floor(Math.random()*(3600-1800+1)+1800);
+final=valor%360;
+console.log("Posición o ángulo Inicial G.A.: "+inicial);
+console.log("Valor Aleatorio Generado en Grados: "+valor);
+console.log("Ángulo Final:"+final);
